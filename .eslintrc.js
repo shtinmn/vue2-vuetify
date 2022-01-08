30991e3d4c6ba19a151a4ee0dd9056815ffe614a
Maksim Shtin <shtinmn@gmail.com>
2022-01-09 02:59:14 +0300

    feat: расширен .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,17 +4,37 @@ module.exports = {
     node: true,
   },
   extends: [
-    "plugin:vue/essential",
-    "eslint:recommended",
-    "@vue/typescript/recommended",
-    "@vue/prettier",
-    "@vue/prettier/@typescript-eslint",
+    'plugin:vue/essential',
+    'eslint:recommended',
+    '@vue/typescript/recommended',
+    '@vue/prettier',
+    '@vue/prettier/@typescript-eslint',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
   ],
   parserOptions: {
     ecmaVersion: 2020,
+    parser: '@typescript-eslint/parser',
+  },
+  plugins: ['import'],
+  settings: {
+    'import/resolver': {
+      alias: {
+        map: [
+          ['@', './src'], //default @ -> ./src alias in Vue, it exists even if vue.config.js is not present
+        ],
+        extensions: ['.vue', '.json', '.js', '.ts'],
+      },
+    },
   },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    'import/order': [
+      'error',
+      {
+        groups: ['index', 'sibling', 'parent', 'internal', 'external', 'builtin', 'object', 'type'],
+        'newlines-between': 'always-and-inside-groups',
+      },
+    ],
   },
-};
+}
