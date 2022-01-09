c600874ca01f3a74082ca475c2b78c258d36cbac
Maksim Shtin <shtinmn@gmail.com>
2022-01-09 21:30:35 +0300

    feat: добавлен vuetify

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import router from './router'
 
 import App from './App.vue'
 
+import vuetify from './plugins/vuetify'
+
 import Vue from 'vue'
 
 import './registerServiceWorker'
@@ -12,5 +14,6 @@ Vue.config.productionTip = false
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App),
 }).$mount('#app')
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,17 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-  </div>
+  <hello-world />
 </template>
 
 <script lang="ts">
-import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
+import HelloWorld from '../components/HelloWorld.vue'
 
-import { Component, Vue } from 'vue-property-decorator'
+import Vue from 'vue'
 
-@Component({
+export default Vue.extend({
+  name: 'Home',
+
   components: {
     HelloWorld,
   },
 })
-export default class Home extends Vue {}
 </script>
