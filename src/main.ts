848f6f9101caf6237ca9e65158398461b3d38510
Maksim Shtin <shtinmn@gmail.com>
2022-01-09 13:09:23 +0300

    refactor: отформатирован проект

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,16 @@
-import Vue from "vue";
-import App from "./App.vue";
-import "./registerServiceWorker";
-import router from "./router";
-import store from "./store";
+import store from './store'
+import router from './router'
 
-Vue.config.productionTip = false;
+import App from './App.vue'
+
+import Vue from 'vue'
+
+import './registerServiceWorker'
+
+Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
-  render: (h) => h(App),
-}).$mount("#app");
+  render: h => h(App),
+}).$mount('#app')
