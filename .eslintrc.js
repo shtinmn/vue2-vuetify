47a32aa22b90b63db4136e59be97f5ab809cee2d
Maksim Shtin <shtinmn@gmail.com>
2022-01-09 23:49:50 +0300

    feat: добавлен vuex-smart-module

--- .eslintrc.js
+++ .eslintrc.js
@@ -37,4 +37,12 @@ module.exports = {
       },
     ],
   },
+  overrides: [
+    {
+      files: ['**/__tests__/*.{j,t}s?(x)', '**/tests/unit/**/*.spec.{j,t}s?(x)'],
+      env: {
+        jest: true,
+      },
+    },
+  ],
 }
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,20 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import { Actions, createStore, Getters, Module, Mutations } from 'vuex-smart-module'
 
-Vue.use(Vuex)
+class RootState {
+  name = 123
+}
 
-export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {},
+class RootGetters extends Getters<RootState> {}
+
+class RootMutations extends Mutations<RootState> {}
+
+class RootActions extends Actions<RootState, RootGetters, RootMutations, RootActions> {}
+
+export const root = new Module({
+  state: RootState,
+  getters: RootGetters,
+  mutations: RootMutations,
+  actions: RootActions,
 })
+
+export default createStore(root)
