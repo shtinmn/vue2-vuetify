e5ae4ad32fb3e59b03bfa6b4c8e9faf77a817f48
Maksim Shtin <shtinmn@gmail.com>
2022-01-09 03:22:10 +0300

    refactor: форматирование

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,8 +6,9 @@
 </template>
 
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
+
+import { Component, Vue } from 'vue-property-decorator'
 
 @Component({
   components: {
